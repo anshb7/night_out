68bbe98bf089e4cd0cc884b0c7955c18e9f9c478
anshb7 <92212789+anshb7@users.noreply.github.com>
2022-07-26 11:52:05 +0530

    fetchingdata

--- lib/provider/products.dart
+++ lib/provider/products.dart
@@ -8,22 +8,38 @@ import './productdetails.dart';
 import '../screens/add_productscreen.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Products with ChangeNotifier {
+  final user = FirebaseAuth.instance.currentUser;
   CollectionReference users = FirebaseFirestore.instance.collection("users");
   List<Product> _items = [];
 
   List<Product> get items {
-    users
-        .snapshots()
-        .map((QuerySnapshot querySnapshot) => querySnapshot.docs.map(
-            (DocumentSnapshot documentSnapshot) => Product(
-                title: documentSnapshot["title"],
-                description: documentSnapshot["description"],
-                price: documentSnapshot["price"],
-                imageUrl: documentSnapshot["imageUrl"])))
-        .toList();
-
     return [..._items];
+  }
+
+  Future getusersdata() async {
+    try {
+      final uid = user!.uid;
+      var data = await FirebaseFirestore.instance
+          .collection("users")
+          .doc(uid)
+          .collection("users")
+          .orderBy("price", descending: true)
+          .get() as Map<String, dynamic>;
+      final List<Product> loadedproducts = [];
+      data.forEach((uid, value) {
+        loadedproducts.add(Product(
+            title: value["title"],
+            description: value["description"],
+            price: value["price"],
+            imageUrl: value["imageUrl"]));
+      });
+      _items = loadedproducts;
+      notifyListeners();
+    } catch (error) {
+      throw error;
+    }
   }
 }
